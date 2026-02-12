cb7c7fe7a2246dda9e27004dbeb710b326053419
Mike Martin <mikemartin@ifb.co.uk>
2026-02-12 13:12:16 +0000

    introduced RothC format weather file generation

--- GlblEcosseSiteSpecSv/wthr_generation_rothc_fns.py
+++ GlblEcosseSiteSpecSv/wthr_generation_rothc_fns.py
@@ -27,13 +27,21 @@ from thornthwaite import thornthwaite
 
 ERROR_STR = '*** Error *** '
 WARNING_STR = '*** Warning *** '
-QUICK_FLAG = False       # forces break from loops after max cells reached in first GCM and SSP
+
 NC_FROM_TIF_FN ='E:\\Saeed\\GSOCmap_0.25.nc'
+METRIC_LIST = list(['precip', 'tas'])
+METRIC_DESCRIPS = {'precip': 'precip = total precipitation (mm)',
+                    'tas': 'tave = near-surface average temperature (degrees Celsius)'}
 
 def generate_rothc_weather(form):
     """
     C
     """
+    out_dir = 'E:\\Saeed\\outputs'
+    if not exists(out_dir):
+        mkdir(out_dir)
+    print('\nWill write Rothc dlimate data to: ' + out_dir)
+
     max_cells = int(form.w_max_cells.text())
 
     # check resolution
@@ -55,58 +63,26 @@ def generate_rothc_weather(form):
     fut_wthr_set = form.weather_set_linkages['WrldClim'][1]
     sim_end_year = form.wthr_sets[fut_wthr_set]['year_end']
 
-    # for each GCM and SSP dataset group e.g. UKESM1-0-LL 585
-    # =======================================================
-    print('')
-    for wthr_set in form.weather_set_linkages['WrldClim']:
-        this_gcm, scnr = wthr_set.split('_')
-        if scnr == 'hist':  # apply filter
-            continue
-        elif scnr != '585':
-            if this_gcm != 'UKESM1-0-LL':
-                continue
-
-        # process complete dataset
-        # ========================
-        wthr_set = form.wthr_sets[wthr_set]
-        strt_year = wthr_set['year_start']
-        end_year  = wthr_set['year_end']
-        ds_precip = wthr_set['ds_precip']
-        ds_tas = wthr_set['ds_tas']
-
-        hist_wthr_dsets, fut_wthr_dsets = dict(), dict()
-        for metric, ds_fname in zip(list(['precip', 'tas']), list(['ds_precip', 'ds_tas'])):
-            # hist_wthr_dsets[metric] = Dataset(hist_wthr_set_defn[ds_fname])
-            fut_wthr_dsets[metric] = Dataset(wthr_set[ds_fname])
-
-        ntotal_wrttn = 0
-        '''
-        for lat_indx, lat in enumerate(wthr_set['longitudes']):
-            for lon_indx, lon in enumerate(wthr_set['latitudes']):
-                ntotal_wrttn += 1
-        '''
-        mess = '\nProcessing weather set: ' + this_gcm + '\tScenario: ' + scnr
-        print(mess)
-        mess = 'Completed weather set: ' + this_gcm + '\tScenario: ' + scnr
-        print(mess + '\tfiles written: ' + format(ntotal_wrttn, ',') + ' \n')
+    this_gcm = form.w_combo10w.currentText()
+    scnr =  form.w_combo10.currentText()
 
     region, crop_name = 2*[None]
     climgen = ClimGenNC(form, region, crop_name, sim_strt_year, sim_end_year, this_gcm, scnr)
     hist_wthr_dsets, fut_wthr_dsets = open_wthr_NC_sets(climgen)
 
-    # check each value, discarding Nans
-    # =================================
+    # check each value, discarding Nans - total size of file is 618 x 1440 = 889,920 cells
+    # ====================================================================================
     dset = Dataset(NC_FROM_TIF_FN)
     latvar = dset.variables['lat']
     lonvar = dset.variables['lon']
     last_time = time()
 
-    nmasked, noutbnds, nnodata, ncmpltd = 4*[0]
+    nmasked, noutbnds, nnodata, ncmpltd, nskipped = 5*[0]
     for lat_indx, lat_ma in enumerate(latvar):
         for lon_indx, lon_ma in enumerate(lonvar):
             lat = lat_ma.item()
             lon = lon_ma.item()
-            last_time = update_wthr_rothc_progress(last_time, nmasked, noutbnds, nnodata, ncmpltd)
+            last_time = update_wthr_rothc_progress(last_time, nmasked, noutbnds, nnodata, ncmpltd, nskipped)
             soil_carb = dset.variables['Band1'][lat_indx][lon_indx]
             if is_masked(soil_carb):
                 val = soil_carb.item()
@@ -119,6 +95,30 @@ def generate_rothc_weather(form):
                 if hist_lat_indx < 0 or fut_lat_indx < 0:
                     noutbnds += 1
                     continue
+
+                # setup output files
+                # ==================
+                grid_coord = '{0:0=5g}_{1:0=5g}'.format(lat_indx, lon_indx)
+
+                wthr_fnames = {}
+                nexist = 0
+                for metric in METRIC_LIST:
+                    wthr_fname = metric + '_' + grid_coord + '.txt'
+                    wthr_fnames[metric] = join(out_dir, wthr_fname)
+                    if exists(wthr_fnames[metric]):
+                        # print('File ' + wthr_fname + ' already exists - will skip')
+                        nexist += 1
+
+                # if both files exist then skip
+                # =============================
+                if nexist == 2:
+                    nskipped += 1
+                    continue
+
+                # weather set lat/lons
+                # ====================
+                lat_wthr = climgen.fut_wthr_set_defn['latitudes'][fut_lat_indx]
+                lon_wthr = climgen.fut_wthr_set_defn['longitudes'][fut_lon_indx]
 
                 # Get future and historic weather data
                 # ====================================
@@ -137,34 +137,74 @@ def generate_rothc_weather(form):
 
                 # create weather
                 # ==============
-                site_obj = MakeSiteFiles(form, climgen)
-                make_rthc_wthr_files(site_obj, lat, lon, climgen, pettmp_hist, pettmp_sim)
+                # site_obj = MakeSiteFiles(form, climgen)
+                make_rthc_wthr_files(wthr_fnames, lat, lat_indx, lon, lon_indx, climgen,
+                                                    lat_wthr, lon_wthr, pettmp_hist, pettmp_sim)
                 ncmpltd += 1
-                ntotal_wrttn += 1
                 if ncmpltd >= max_cells:
                     break
 
     dset.close()
 
-    print('Finished RothC weather generation - total number of sets written: {}'.format(ntotal_wrttn))
+    print('Finished RothC weather generation - total number of sets written: {}'.format(ncmpltd))
 
     return
 
-def make_rthc_wthr_files(site, lat, lon, climgen, pettmp_hist, pettmp_sim):
+def make_rthc_wthr_files(wthr_fnames, lat, lat_indx, lon, lon_indx, climgen, lat_wthr, lon_wthr, pettmp_hist, pettmp_sim):
     """
     write a RothC weather dataset
     """
-    out_dir = 'E:\\Saeed\\outputs'
-    if not exists(out_dir):
-        mkdir(out_dir)
+    hdr_recs = _fetch_header_recs(lat, lat_indx, lon, lon_indx, climgen, lat_wthr, lon_wthr)
+    frst_rec, period, location_rec, box_rec, grid_ref_rec = hdr_recs
 
-    with open(join(out_dir, 'fnames.txt'), 'w') as fobj:
-        fobj.write('From Wthe orldClim database of high spatial resolution global weather and climate data.')
-        fobj.write('\n.tave = near-surface average temperature (degrees Celsius)')
-        fobj.write('\nPeriod=2021-2120 Variable=.tave')
-        fobj.write('\n[Long= -11.00,  32.00] [Lati=  34.00,  72.00] [Grid X,Y= 258, 228]')
-        fobj.write('\n[Boxes=   31143] [Years=2021-2120] [Multi=    0.1000] [Missing=-999]')
-        fobj.write('\nGrid-ref=   4, 109')
-        fobj.flush()
+    for metric in METRIC_LIST:
+        metric_descr = METRIC_DESCRIPS[metric]
+        data_recs = _generate_data_recs(pettmp_sim[metric])
+
+        with open(wthr_fnames[metric], 'w') as fobj:
+            fobj.write(frst_rec)
+            fobj.write('\n.' + metric_descr)
+            fobj.write('\nPeriod=' + period + ' Variable=.' + metric)
+            fobj.write('\n' + location_rec)
+            fobj.write('\n' + box_rec)
+            fobj.write('\n' + grid_ref_rec)
+            for data_rec in data_recs:
+                fobj.write('\n' + data_rec)
+            fobj.flush()
 
     return
+
+def _fetch_header_recs(lat, lat_indx, lon, lon_indx, climgen, lat_wthr, lon_wthr):
+    """
+    create strings for header records
+    From the WorldClim database of high spatial resolution global weather and climate data.
+    """
+    gcm = climgen.wthr_rsrce
+    scnr = climgen.fut_clim_scen
+    frst_rec = 'From the WorldClim database of global weather and climate data using GCM: '
+    frst_rec += gcm + '\tScenario: ' + scnr
+    period = str(climgen.fut_wthr_set_defn['year_start']) + '-' + str(climgen.fut_wthr_set_defn['year_end'])
+    location_rec = '[Long= ' + str(round(lon, 3)) + ', ' + str(round(lon_wthr, 3))
+    location_rec += '] [Lati= ' + str(round(lat, 3)) + ', ' + str(round(lat_wthr))
+    location_rec +=  '] [Grid X,Y= ' + str(lon_indx) + ', ' + str(lat_indx) + ']'
+    box_rec = '[Boxes=   31143] [Years=' + period + '] [Multi=    0.1000] [Missing=-999]'
+    grid_ref_rec = 'Grid-ref=  ' + str(lon_indx) + ', ' + str(lat_indx)
+
+    return (frst_rec, period, location_rec, box_rec, grid_ref_rec)
+
+def _generate_data_recs(pettmp):
+    """
+    create strings for header records
+    """
+    nvals = len(pettmp)
+    rec_list = []
+    for indx in range(0, nvals, 12):
+        vals_yr = pettmp[indx: indx + 12]
+        rec_str = str([round(val, 2) for val in vals_yr])
+        rec_str = rec_str.replace(', ', '\t')
+        rec_str = rec_str.replace(', ', '\t')
+        rec_str = rec_str.replace('[','')
+        rec_str = rec_str.replace(']','')
+        rec_list.append(rec_str)
+
+    return rec_list
